5d9af63f6f3ba8448e44aaf0a1d41ff3f18a56d5
Tran Phan <tran.phan@saver.jp>
2017-07-13 16:25:40 +0700

    header +menu

--- wp-content/themes/anhkietphat/functions.php
+++ wp-content/themes/anhkietphat/functions.php
@@ -26,10 +26,10 @@ if (!isset($content_width)) {
 /**
  * Khai bao chuc nang cua theme
  */
-if (!function_exists('anhkietphat_theme_setup')) {
+if (!function_exists('theme_setup')) {
 
 
-    function anhkietphat_theme_setup()
+    function theme_setup()
     {
         /* Thiet lap textdomain */
         $language_folder = THEME_URL . '/languages';
@@ -72,7 +72,7 @@ if (!function_exists('anhkietphat_theme_setup')) {
 
     }
 
-    add_action('init', 'anhkietphat_theme_setup');
+    add_action('init', 'theme_setup');
 
 }
 
@@ -97,3 +97,46 @@ if (!function_exists('site_name_header')) {
     }
 }
 
+/*
+ * load theme css
+ */
+if (!function_exists('theme_scripts')) {
+    function theme_scripts()
+    {
+        wp_enqueue_style('responsive', THEME_URL . "/css/responsive.css");
+        wp_enqueue_style('responsive', THEME_URL . "/css/bootstrap.min.css");
+        wp_enqueue_script( 'custom_js', THEME_URL . '/js/jquery.min.js' );
+        wp_enqueue_script( 'custom_js', THEME_URL . '/js/bootstrap.min.js' );
+        wp_enqueue_script( 'custom_js', THEME_URL . '/js/custom.js' );
+    }
+
+    add_action('wp_enqueue_scripts', 'theme_scripts');
+}
+wp_enqueue_style('style', get_stylesheet_uri());
+
+/*
+ * pagination
+ */
+if (!function_exists('pagination')) {
+    function pagination()
+    {
+        global $wp_query;
+        $big = 12345678;
+        $page_format = paginate_links( array(
+            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+            'format' => '?paged=%#%',
+            'current' => max( 1, get_query_var('paged') ),
+            'total' => $wp_query->max_num_pages,
+            'type'  => 'array'
+        ) );
+        if( is_array($page_format) ) {
+            $paged = ( get_query_var('paged') == 0 ) ? 1 : get_query_var('paged');
+            echo '<div class="pagination"><div><ul>';
+            echo '<li><span>'. $paged . ' of ' . $wp_query->max_num_pages .'</span></li>';
+            foreach ( $page_format as $page ) {
+                echo "<li>$page</li>";
+            }
+            echo '</ul></div></div>';
+        }
+    }
+}
--- wp-content/themes/anhkietphat/header.php
+++ wp-content/themes/anhkietphat/header.php
@@ -2,56 +2,81 @@
 <html <?php language_attributes(); ?> />
 
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="profile" href="http://gmgp.org/xfn/11" />
-    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <meta charset="<?php bloginfo('charset'); ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <link rel="profile" href="http://gmgp.org/xfn/11"/>
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
     <?php wp_head(); ?>
 </head>
-<body <?php body_class();?> >
+<body <?php body_class(''); ?> <?php if (!is_home() || !is_front_page()) { ?>id="innerPage"<?php } ?>>
 <div class="header">
     <div class="header-inner">
         <div class="logo">
-            <a href="<?php echo esc_url( home_url('/') ); ?>">
+            <a href="<?php echo esc_url(home_url('/')); ?>">
                 <h1><span>A</span>NH KIET PHAT</h1>
-                <span class = "tagline">Nhà thầu của mọi công trình</span>
-                </a>
-        </div> <!-- logo -->
-        <?php if ( ! dynamic_sidebar('sidebar-header')): ?>
-
-            <div class="column-1">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/icon-phone.jpg" alt="" />
-                <?php if( '' !== get_theme_mod('head_number')){ ?>
-                    <span class="headerphone"><?php echo esc_attr( get_theme_mod('head_number', '0789 256 321', 'skt-construction' )); ?></span><br>
-                <?php } ?>
-                <?php if( '' !== get_theme_mod('head_email')){ ?>
-                    <span class="headreemail"><a href="mailto:<?php echo sanitize_email(get_theme_mod('head_email','info@buildpress.com')); ?>"><?php echo esc_attr(get_theme_mod('head_email','info@buildpress.com')); ?></a></span>
-                <?php } ?>
-                <div class="clear"></div>
-            </div>
-
-            <div class="column-2">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/icon-home.jpg" alt="" />
-                <?php if( '' !== get_theme_mod('head_address')){ ?>
-                    <span class="headerphone"><?php echo esc_attr( get_theme_mod('head_address', 'Street 238, Perth,', 'skt-construction' )); ?></span><br>
-                <?php } ?>
-                <?php if( '' !== get_theme_mod('head_citycountry')){ ?>
-                    <span class="headreemail"><?php echo esc_attr( get_theme_mod('head_citycountry', 'Western Australia', 'skt-construction' )); ?></span>
-                <?php } ?>
-                <div class="clear"></div>
-            </div>
-        <?php endif; ?>
-        <div class="column-3">
-            <?php if ( '' !== get_theme_mod( 'fb_link' ) ) { ?>
-                <a title="facebook" class="fb" target="_blank" href="<?php echo esc_url(get_theme_mod('fb_link','http://www.facebook.com')); ?>"></a>
-            <?php } ?>
-            <?php if ( '' !== get_theme_mod('gplus_link') ) { ?>
-                <a title="google-plus" class="gp" target="_blank" href="<?php echo esc_url(get_theme_mod('gplus_link','http://plus.google.com')); ?>"></a>
-            <?php }?>
-            <?php if ( '' !== get_theme_mod('linked_link') ) { ?>
-                <a title="linkedin" class="in" target="_blank" href="<?php echo esc_url(get_theme_mod('linked_link','http://www.linkedin.com')); ?>"></a>
-            <?php } ?>
+                <span class="tagline">Nhà thầu của mọi công trình</span>
+            </a>
         </div>
-    </div><!-- .headerright -->
+        <!-- logo -->
+        <div class="headerright">
+            <?php if (!dynamic_sidebar('sidebar-header')): ?>
+
+                <div class="column-1">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icon-phone.jpg" alt=""/>
+                    <?php if ('' !== get_theme_mod('head_number')) { ?>
+                        <span
+                            class="headerphone"><?php echo esc_attr(get_theme_mod('head_number', '0912 345 678', 'skt-construction')); ?></span>
+                        <br>
+                    <?php } ?>
+                    <?php if ('' !== get_theme_mod('head_email')) { ?>
+                        <span class="headreemail"><a
+                                href="mailto:<?php echo sanitize_email(get_theme_mod('head_email', 'anhkietphat@gmail.com')); ?>"><?php echo esc_attr(get_theme_mod('head_email', 'anhkietphat@gmail.com')); ?></a></span>
+                    <?php } ?>
+                    <div class="clear"></div>
+                </div>
+
+                <div class="column-2">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icon-home.jpg" alt=""/>
+                    <?php if ('' !== get_theme_mod('head_address')) { ?>
+                        <span
+                            class="headerphone"><?php echo esc_attr(get_theme_mod('head_address', '02 Đống Đa,', 'skt-construction')); ?></span>
+                        <br>
+                    <?php } ?>
+                    <?php if ('' !== get_theme_mod('head_citycountry')) { ?>
+                        <span
+                            class="headreemail"><?php echo esc_attr(get_theme_mod('head_citycountry', 'Hải Châu, Đà Nẵng', 'skt-construction')); ?></span>
+                    <?php } ?>
+                    <div class="clear"></div>
+                </div>
+            <?php endif; ?>
+            <div class="column-3">
+                <?php if ('' !== get_theme_mod('fb_link')) { ?>
+                    <a title="facebook" class="fb" target="_blank"
+                       href="<?php echo esc_url(get_theme_mod('fb_link', 'http://www.facebook.com')); ?>"></a>
+                <?php } ?>
+                <?php if ('' !== get_theme_mod('gplus_link')) { ?>
+                    <a title="google-plus" class="gp" target="_blank"
+                       href="<?php echo esc_url(get_theme_mod('gplus_link', 'http://plus.google.com')); ?>"></a>
+                <?php } ?>
+                <?php if ('' !== get_theme_mod('linked_link')) { ?>
+                    <a title="youtube" class="yt" target="_blank"
+                       href="<?php echo esc_url(get_theme_mod('yt_link', 'https://www.youtube.com/')); ?>"></a>
+                <?php } ?>
+            </div>
+        </div>
+        <!-- headerright -->
+
+
+        <div class="clear"></div>
+        <div class=toggle><a class="toggleMenu" href="<?php echo esc_url('#'); ?>"><?php _e('Menu', 'menu'); ?></a>
+        </div>
+        <div class="nav">
+            <?php wp_nav_menu(array('theme_location' => 'primary-menu')); ?>
+        </div>
+
     </div>
+    <!-- header-inner -->
 </div>
+<!-- header -->
+
+
